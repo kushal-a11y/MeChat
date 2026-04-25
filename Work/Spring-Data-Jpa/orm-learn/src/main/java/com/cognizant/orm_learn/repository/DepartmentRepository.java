2500de6095f86fa73b261e96d69eed5c6ce21886
kushal-a11y <kushalstcet666@gmail.com>
2026-04-25 12:23:20 +0530

    hu hu ha ha

--- Work/Spring-Data-Jpa/orm-learn/src/main/java/com/cognizant/orm_learn/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.orm_learn.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognizant.orm_learn.model.Department;
-
-public interface DepartmentRepository extends JpaRepository<Department, Integer>{
-    
-}
--- Work/Spring-Data-Jpa/orm-learn/src/main/java/com/cognizant/orm_learn/repository/SkillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.orm_learn.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognizant.orm_learn.model.Skill;
-
-public interface SkillRepository extends JpaRepository<Skill, Integer>{
-    
-}
